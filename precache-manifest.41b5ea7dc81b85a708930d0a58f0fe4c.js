23834c17dddd10dd9c01b7b473cd2e4a5054bca7
Daniil Molodkov <dmolodkov@brainstation.io>
2020-06-15 07:08:45 -0400

    Updates

---
+++ precache-manifest.41b5ea7dc81b85a708930d0a58f0fe4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ba6d5faee3e2eed6e733c6e26b1881b",
+    "url": "/react-github-pages/index.html"
+  },
+  {
+    "revision": "10b76ef7d4e63c7f4e8d",
+    "url": "/react-github-pages/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f2cfa86372125d473b12",
+    "url": "/react-github-pages/static/js/2.57dfd8ed.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-github-pages/static/js/2.57dfd8ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10b76ef7d4e63c7f4e8d",
+    "url": "/react-github-pages/static/js/main.b8365417.chunk.js"
+  },
+  {
+    "revision": "c1cd3223d81e68d4852c",
+    "url": "/react-github-pages/static/js/runtime-main.edb4c5c5.js"
+  }
+]);
